acbce6312fe570eb15438038844fd1f901b6948a
mimme <mirzamerdovic@gmail.com>
2021-06-03 23:44:31 +0200

    Refactoring

--- src/Requester/Program.cs
+++ src/Requester/Program.cs
@@ -2,62 +2,74 @@
 using Bunny.Messages;
 using RabbitMQ.Client;
 using System;
-using System.Text;
 using System.Text.Json;
 
 namespace Requester
 {
     internal static class Program
     {
+        private const string MODE = "direct";
+
+        // Direct mode
         private const string REQUESTS_QUEUE = "generation_requests";
+
+        // Topics mode
+        private const string EXCHANGE = "generations";
+        private const string EXCHANGE_TYPE = "topic";
+        private const string ROUTING_KEY = "generation.requests";
 
         internal static void Main(string[] args)
         {
             using var connection = RmqConnectionFactory.Create();
             using var channel = connection.CreateModel();
 
-            channel.QueueDeclare(queue: REQUESTS_QUEUE, durable: true, exclusive: false, autoDelete: false, arguments: null);
+            if (MODE == "direct")
+            {
+                channel.QueueDeclare(queue: REQUESTS_QUEUE, durable: true, exclusive: false, autoDelete: false, arguments: null);
 
+                var (message, properties) = CreateMessageAndProperties(channel);
+                var body = JsonSerializer.SerializeToUtf8Bytes(message);
+
+                channel.BasicPublish(
+                    exchange: "",
+                    routingKey: REQUESTS_QUEUE,
+                    basicProperties: properties,
+                    body: body);
+
+                Console.WriteLine(" [x] Sent '{0}':'{1}' CID: {2}", REQUESTS_QUEUE, message.Id, properties.CorrelationId);
+            }
+            else if (MODE == "topic")
+            {
+                channel.ExchangeDeclare(exchange: EXCHANGE, type: EXCHANGE_TYPE, durable: true);
+
+                var (message, properties) = CreateMessageAndProperties(channel);
+                var body = JsonSerializer.SerializeToUtf8Bytes(message);
+
+                channel.BasicPublish(
+                    exchange: EXCHANGE,
+                    routingKey: ROUTING_KEY,
+                    basicProperties: properties,
+                    body: body);
+
+                Console.WriteLine(" [x] Sent '{0}':'{1}' CID: {2}", ROUTING_KEY, message.Id, properties.CorrelationId);
+            }
+            else
+                throw new NotImplementedException(MODE);
+        }
+
+        private static (RequestGeneration message, IBasicProperties properties) CreateMessageAndProperties(IModel channel)
+        {
             var message = new RequestGeneration
             {
                 Id = new Random().Next(1000),
                 Steps = new[] { Step.GenerateImagePreview, Step.GenerateMetadata }
             };
 
-            var body = JsonSerializer.SerializeToUtf8Bytes(message);
-
             var properties = channel.CreateBasicProperties();
             properties.Persistent = true;
             properties.CorrelationId = Guid.NewGuid().ToString();
 
-            channel.BasicPublish(exchange: "", routingKey: REQUESTS_QUEUE, basicProperties: properties, body: body);
-            Console.WriteLine(" [x] Sent {0}", message);
-        }
-
-        private const string EXCHANGE = "generations";
-        private const string EXCHANGE_TYPE = "topic";
-        private const string ROUTING_KEY = "generation.requests";
-
-        internal static void Main2(string[] args)
-        {
-            using var connection = RmqConnectionFactory.Create();
-            using var channel = connection.CreateModel();
-
-            channel.ExchangeDeclare(exchange: EXCHANGE, type: EXCHANGE_TYPE, durable: true);
-
-            var message = "1";
-            var body = Encoding.UTF8.GetBytes(message);
-            var properties = channel.CreateBasicProperties();
-            properties.CorrelationId = Guid.NewGuid().ToString();
-
-            channel.BasicPublish(
-                exchange: EXCHANGE,
-                routingKey: ROUTING_KEY,
-                basicProperties: properties,
-                body: body);
-
-            Console.WriteLine(" [x] Sent '{0}':'{1}' CID: {2}", ROUTING_KEY, message, properties.CorrelationId);
-            Console.ReadKey();
+            return (message, properties);
         }
     }
 }
--- src/Supervisor/Program.cs
+++ src/Supervisor/Program.cs
@@ -4,88 +4,79 @@ using Microsoft.Toolkit.HighPerformance;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 using System;
-using System.Text;
 using System.Text.Json;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace Supervisor
 {
     internal static class Program
     {
+        private const string MODE = "direct";
+
+        // Direct mode
         private const string REQUESTS_QUEUE = "generation_requests";
 
-        internal static void Main(string[] args)
-        {
-            using var connection = RmqConnectionFactory.Create();
-            using var channel = connection.CreateModel();
-
-            channel.QueueDeclare(queue: REQUESTS_QUEUE, durable: true, exclusive: false, autoDelete: false, arguments: null);
-
-            channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
-
-            Console.WriteLine(" [*] Waiting for messages.");
-
-            var consumer = new AsyncEventingBasicConsumer(channel);
-
-            consumer.Received += async (model, ea) =>
-            {
-                var message = await JsonSerializer.DeserializeAsync<RequestGeneration>(ea.Body.AsStream());
-                var correlationId = ea.BasicProperties.CorrelationId;
-
-                Console.WriteLine(" [x] Received {0}. CID: {1}", message, correlationId);
-
-                var c = ((AsyncEventingBasicConsumer)model).Model;
-                c.BasicAck(ea.DeliveryTag, false);
-
-                await Task.Yield();
-            };
-
-            channel.BasicConsume(queue: REQUESTS_QUEUE, autoAck: false, consumer: consumer);
-
-            Console.WriteLine(" Press [enter] to exit.");
-            Console.ReadLine();
-        }
-
+        // Topics mode
         private const string EXCHANGE = "generations";
         private const string EXCHANGE_TYPE = "topic";
         private const string BINDING_KEY = "generation.requests";
 
-        internal static void Main2(string[] args)
+        internal static void Main(string[] args)
         {
-            using var connection = RmqConnectionFactory.Create();
-            using var channel = connection.CreateModel();
+            var connection = RmqConnectionFactory.Create();
+            var channel = connection.CreateModel();
 
-            channel.ExchangeDeclare(exchange: EXCHANGE, type: EXCHANGE_TYPE, durable: true);
-            var queueName = channel.QueueDeclare().QueueName;
-
-            channel.QueueBind(queue: queueName, exchange: EXCHANGE, routingKey: BINDING_KEY);
-
-            Console.WriteLine(" [*] Waiting for messages. To exit press CTRL+C");
-
-            var consumer = new EventingBasicConsumer(channel);
-
-            consumer.Received += (model, ea) =>
+            if (MODE == "direct")
             {
-                var message = Encoding.UTF8.GetString(ea.Body.ToArray());
-                var routingKey = ea.RoutingKey;
-                var correlationId = ea.BasicProperties.CorrelationId;
+                channel.QueueDeclare(queue: REQUESTS_QUEUE, durable: true, exclusive: false, autoDelete: false, arguments: null);
+                channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
 
-                ProcessMessage(correlationId, message);
-                Console.WriteLine(" [x] Received '{0}':'{1}' CID: {2}", routingKey, message, correlationId);
-            };
+                Console.WriteLine(" [*] Waiting for messages.");
 
-            channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
+                var consumer = new AsyncEventingBasicConsumer(channel);
+                consumer.Received += async (model, ea) => await ProcessMessage(model, ea);
 
-            // TODO: Create a new channel that will listen on generations.step-completed
+                channel.BasicConsume(queue: REQUESTS_QUEUE, autoAck: false, consumer: consumer);
+            }
+            else if (MODE == "topic")
+            {
+                channel.ExchangeDeclare(exchange: EXCHANGE, type: EXCHANGE_TYPE, durable: true);
+                var queueName = channel.QueueDeclare().QueueName;
+                channel.QueueBind(queue: queueName, exchange: EXCHANGE, routingKey: BINDING_KEY);
+
+                Console.WriteLine(" [*] Waiting for messages. To exit press CTRL+C");
+
+                var consumer = new AsyncEventingBasicConsumer(channel);
+                consumer.Received += async (model, ea) => await ProcessMessage(model, ea);
+
+                channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
+            }
+            else
+                throw new NotImplementedException(MODE);
 
             Console.WriteLine(" Press [enter] to exit.");
             Console.ReadLine();
+
+            channel.Close();
+            connection.Close();
         }
 
-        private static void ProcessMessage(string correlationId, string message)
+        private static async Task ProcessMessage(object model, BasicDeliverEventArgs ea)
         {
-            // TODO: send a message to requires step processors
+            using var body = ea.Body.AsStream();
+            var message = await JsonSerializer.DeserializeAsync<RequestGeneration>(body);
+
+            // Do something with the message
+
+            var correlationId = ea.BasicProperties.CorrelationId;
+            var sourceQueue = ea.RoutingKey;
+            Console.WriteLine(" [x] Received '{0}':'{1}' CID: {2}", sourceQueue, message.Id, correlationId);
+
+            if (MODE == "direct")
+            {
+                var c = ((AsyncEventingBasicConsumer)model).Model;
+                c.BasicAck(ea.DeliveryTag, false);
+            }
         }
     }
 }
